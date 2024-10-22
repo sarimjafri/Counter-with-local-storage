d64f3673abe6520428c04dbe6b4efee70539225c
MasirJafri1 <masirjafri1@gmail.com>
2024-10-23 02:21:29 +0530

    Decreament button also added

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -11,6 +11,11 @@ const Counter = () => {
   }, [counter]);
 
   const incrementCounter = () => setCounter(counter + 1);
+  const decrementCounter = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  };
   const resetCounter = () => setCounter(0);
 
   return (
@@ -24,6 +29,14 @@ const Counter = () => {
         className="px-8 py-4 bg-blue-600 text-white font-semibold rounded-full hover:bg-blue-700 transition transform hover:scale-105 focus:ring-4 focus:ring-blue-300 mb-4 mr-4"
       >
         Increment
+      </button>
+
+      {/* Decrement Button */}
+      <button
+        onClick={decrementCounter} // onClick for decrement
+        className="px-8 py-4 bg-yellow-600 text-white font-semibold rounded-full hover:bg-yellow-700 transition transform hover:scale-105 focus:ring-4 focus:ring-yellow-300 mb-4 mr-4"
+      >
+        Decrement
       </button>
 
       {/* Reset Button */}
